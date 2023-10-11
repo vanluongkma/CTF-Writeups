6af751b424e93b3cf82c87aa7a19611fc1062d62
patriot <127461439+piropatriot@users.noreply.github.com>
2023-10-11 14:36:24 +0700

    Create Crypto 02 - Encoding 2.py

---
+++ OLICYBER/Cryptography/Crypto 02 - Encoding 2.py
@@ -0,0 +1,6 @@
+from sympy.ntheory.residue_ntheory import discrete_log
+
+flag = "666c61677b68337834646563696d616c5f63346e5f62335f41424144424142457d"
+print(bytes.fromhex(flag))
+
+# flag{h3x4decimal_c4n_b3_ABADBABE}
